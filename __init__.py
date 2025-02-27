3c6e2e6c04bec037425d94e4b4a03bf8cec495b9
chenpipi0807 <144689950+chenpipi0807@users.noreply.github.com>
2025-02-27 18:08:35 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,12 +1,15 @@
 from .dynamic_judgment_node import DynamicJudgmentNode
+from .prompt_judgment_node import PromptJudgmentNode
 
 NODE_CLASS_MAPPINGS = {
-    "DynamicJudgmentNode": DynamicJudgmentNode
+    "DynamicJudgmentNode": DynamicJudgmentNode,
+    "PromptJudgmentNode": PromptJudgmentNode
 }
 
 # Make sure to use matching names
 NODE_DISPLAY_NAME_MAPPINGS = {
-    "DynamicJudgmentNode": "图像动态判断"
+    "DynamicJudgmentNode": "图像动态判断",
+    "PromptJudgmentNode": "prompt动态判断"
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
